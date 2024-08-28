16981150d32d405de59498206516b2ed257f681a
Harold <haroldnixon@gmail.com>
2024-08-28 12:04:36 -0400

    Mini1

---
+++ js/app.js
@@ -0,0 +1,85 @@
+// document.getElementById("belcherfamily").textContent="Bob";
+
+// Big / Little image
+function bigImg(x) {
+    x.style.height = "180px";
+    x.style.width = "180px";
+  }
+  
+  function normalImg(x) {
+    x.style.height = "150px";
+    x.style.width = "150px";
+  }
+// Big / Little END
+
+// const name = ["Bob", "Linda", "Tina", "Gene", "Louise"];
+
+
+// Pictures START
+function myOverBelcher(document) {
+    document.getElementById("belcherfamily").textContent="The Belcher Family";
+}
+
+function myOverBob(document) {
+    document.getElementById("belcherfamily").textContent="Bob Belcher";
+}
+
+function myOverLinda(document) {
+    document.getElementById("belcherfamily").textContent="Linda Belcher";
+}
+
+function myOverLouise(document) {
+    document.getElementById("belcherfamily").textContent="Louise Belcher";
+}
+
+function myOverGene(document) {
+    document.getElementById("belcherfamily").textContent="Gene Belcher";
+}
+
+function myOverTina(document) {
+    document.getElementById("belcherfamily").textContent="Tina Belcher";
+}
+// Pictures END
+
+// Audio START
+
+function playAudioBob(bobButton) { 
+    var audio = document.getElementById("audio"); 
+    audioBob.play(); 
+} 
+
+function playAudioLinda(lindaButton) { 
+    var audio = document.getElementById("audioLinda"); 
+    audioLinda.play(); 
+} 
+
+
+
+
+
+//function myOverFunctionBelcher(document) {
+//    document.getElementById("belcherfamily").textContent="The Belcher Family!!!";
+//}
+/*
+//let belchername = "The Belcher Family VAR";
+    switch ( belchername ==  ) {      
+    case 0:                         
+        belchername = "Bob Belcher";          
+        break;
+    case 1:                         
+        belchername = "Linda Belcher";
+        break;
+    case 2:                         
+        text = "Tina";
+        break;
+    case 3:                         
+        text = "Gene";
+        break;
+    case 4:                         
+        text = "Louise";
+        break;
+    default:                        // else...
+        text = "The Belcher Family";
+    } 
+
+*/
